40beb069583c8a8bf2e41ea934f8a1149c1012b8
yyandrew <qqerqqer@126.com>
2018-06-20 16:03:10 +0800

    MISC - Additional App Organization - Create new landingPage/Pricing component - Create new landingPage/Pitch component

--- app/javascript/packs/exampleApp/components/landingPage.jsx
+++ app/javascript/packs/exampleApp/components/landingPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Pitch from './landingPage/pitch';
+import Pricing from './landingPage/pricing';
 
 export default class LandingPage extends React.Component {
   render () {
     return (
       <div>
-        <h1>Hello World</h1>
+        <Pitch {...this.props} />
+        <Pricing {...this.props} />
       </div>
     )
   }
