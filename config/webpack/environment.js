f3f40499d0d990164bb829d71aa61554b7e54f3d
yyandrew <qqerqqer@126.com>
2018-06-20 15:09:53 +0800

    MISC - Configure Webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,27 @@
 const { environment } = require('@rails/webpacker')
 
+module: {
+  // configuration regarding modules
+  rules: [
+    // rules for modules(configure exclude, loader, parser options)
+    {
+      test: /\.js(\.erb)?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          ['env', { modules: false }]
+        ]
+      }
+    }
+  ]
+}
+
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+})
+
 module.exports = environment
